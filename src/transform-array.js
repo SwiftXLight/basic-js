57195966cc7c3b344e45dd99423d89fadc9e57cd
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-09-17 13:09:16 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw 'Error';
+  let tempArr = [];
+ 
+  for (i=0; i < arr.length; i++) {
+      if (arr[i] === '--discard-next') {
+          if (i < arr.length - 1) {
+            i++;
+          }
+      } else if (arr[i] === '--double-next') {
+        if(i < arr.length - 1){
+          tempArr.push(arr[i + 1]);
+        } 
+      } else if (arr[i] === '--discard-prev') {
+          if(i > 0 && arr[i - 2] !== '--discard-next'){
+            tempArr.pop();
+          }
+      } else if (arr[i] === '--double-prev') {
+          if(i > 0 && arr[i - 2] !== '--discard-next'){
+            tempArr.push(arr[i - 1]);
+          }
+      } else {
+          tempArr.push(arr[i]);
+      }     
+      }
+  return tempArr;
 }
