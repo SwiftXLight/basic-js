44d63430190a05580e96a38d8d125583ab660096
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-09-17 13:05:38 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity ) {
+  let half = 0.693 / HALF_LIFE_PERIOD;
+  let age;
+  if (sampleActivity > 15 || sampleActivity <= 0 || typeof(sampleActivity) != 'string' || sampleActivity === 'undefined' || isNaN(sampleActivity))
+    return false;
+  age = Math.ceil(Math.log(MODERN_ACTIVITY/+sampleActivity)/half);
+  return age;
 }
