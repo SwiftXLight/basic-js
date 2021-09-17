722d44f885051c3a58b06775c4540b569791439b
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-09-17 13:11:05 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let string = str+'';
+  options.separator = options.separator || '+';
+  options.additionSeparator = options.additionSeparator || '|'
+  let addition = ''
+  if(options.additionRepeatTimes>1 && options.addition!== undefined){
+      options.addition = options.addition + options.additionSeparator;
+      addition = options.addition.repeat(options.additionRepeatTimes);
+      addition = addition.slice(0, addition.length-options.additionSeparator.length);
+  }
+  else if (options.addition){
+      addition = options.addition;
+  }
+  if(options.repeatTimes){
+      string = string + addition + options.separator;
+      string = string.repeat(options.repeatTimes);
+      string = string.slice(0, string.length-options.separator.length);
+  }
+  else {
+      string = string + addition;
+  }
+  return string;
 }
